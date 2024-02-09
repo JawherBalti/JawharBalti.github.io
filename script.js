1c8a6643631dcf8725cb108e502b7857f5895409
jawherbalti <jawher-balti@live.com>
2024-02-09 20:20:45 +0100

    snap scroll

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ footer.appendChild(text);
 
 const slides = document.querySelector(".slides");
 const scrollUp = document.querySelector(".scroll__up");
-// const scrollDown = document.querySelector(".scroll__down");
 
 const aboutMeLink = document.querySelector("#link__aboutme");
 const projectsLink = document.querySelector("#link__projects");
@@ -13,7 +12,7 @@ const skillsLink = document.querySelector("#link__skills");
 const contactLink = document.querySelector("#link__contact");
 
 const navBar = document.querySelector("nav");
-const firstPage = document.querySelector('.info__container');
+const firstPage = document.querySelector(".info__container");
 const navLinksSmall = document.querySelector(".nav__links");
 const aboutMeLinkSmall = document.querySelector("#link__aboutme--small");
 const projectsLinkSmall = document.querySelector("#link__projects--small");
@@ -61,257 +60,99 @@ window.addEventListener("touchmove", function (e) {
   }, 100);
 });
 
-
-// function updateNavbarVisibility() {
-//   const rect = firstPage.getBoundingClientRect();
-//   const secondPageTop = rect.top + window.scrollY;
-
-//   if (window.scrollY < secondPageTop) {
-//   // User has scrolled past the first page
-//   navBar.classList.add('hidden');
-//   navBar.classList.remove("show__nav__down");
-//   navBar.classList.add("show__nav__up");
-// } else {
-//   // User is on the first page
-//   navBar.classList.remove('hidden');
-//   navBar.classList.add("show__nav__down");
-//   navBar.classList.remove("show__nav__up");
-// }
-// }
-
-// updateNavbarVisibility();
-
-// Add event listeners for both 'wheel' and 'scroll' events
-// window.addEventListener('wheel', updateNavbarVisibility);
-// window.addEventListener('scroll', updateNavbarVisibility);
-
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
 });
-//window.addEventListener("wheel", (e) => {
 
-
-  // if(window.scrollY > window.innerHeight) {
-  //   navBar.classList.add("hidden");
-  //   navBar.classList.remove("show__nav__down");
-  //   navBar.classList.add("show__nav__up");
-  // }else {
-  //   navBar.classList.remove("hidden");
-  //   navBar.classList.add("show__nav__down");
-  //   navBar.classList.remove("show__nav__up");
-  // }
-
-  // if (e.deltaY > 0) {
-  //   activeSlideIndex++;
-  //   if (activeSlideIndex > 4) activeSlideIndex = 4;
-  // } else {
-  //   activeSlideIndex--;
-  //   if (activeSlideIndex < 0) activeSlideIndex = 0;
-  // }
-
-//});
-// let wheeling = false;
-// var scrollTimer = false;
-// window.addEventListener("wheel", function (e) {
-
-//   if (!wheeling) {
-//     if (e.deltaY > 0) changeSlide("down");
-//     if (e.deltaY < 0) changeSlide("up");
-//     wheeling = true;
-//   }
-//   window.clearInterval(scrollTimer);
-//   scrollTimer = this.window.setTimeout(function () {
-//     wheeling = false;
-//   }, 100);
-// });
-
-// scrollUp.addEventListener("click", () => {
-
-// });
-// scrollDown.addEventListener("click", () => {
-
-// });
-
-// window.addEventListener("keydown", (e) => {
-//   switch (e.key) {
-//     case "ArrowDown":
-//       changeSlide("down");
-//       break;
-//     case "ArrowUp":
-//       changeSlide("up");
-//       break;
-//   }
-// });
-
-// aboutMeLink.addEventListener("click", () => {
-//   slides.style.transform = `translateY(-${100}vh)`;
-//   activeSlideIndex = 1;
-//   scrollUp.classList.remove("hidden");
-//   scrollDown.classList.remove("hidden");
-// });
-
-// projectsLink.addEventListener("click", () => {
-//   slides.style.transform = `translateY(-${200}vh)`;
-//   activeSlideIndex = 2;
-//   scrollUp.classList.remove("hidden");
-//   scrollDown.classList.remove("hidden");
-// });
-
-// skillsLink.addEventListener("click", () => {
-//   slides.style.transform = `translateY(-${300}vh)`;
-//   activeSlideIndex = 3;
-//   scrollUp.classList.remove("hidden");
-//   scrollDown.classList.remove("hidden");
-// });
-
-// contactLink.addEventListener("click", () => {
-//   slides.style.transform = `translateY(-${400}vh)`;
-//   activeSlideIndex = 4;
-//   scrollUp.classList.remove("hidden");
-//   scrollDown.classList.add("hidden");
-// });
-
- aboutMeLinkSmall.addEventListener("click", () => {
+aboutMeLinkSmall.addEventListener("click", () => {
   navLinksSmall.classList.remove("show__menu");
   navLinksSmall.classList.add("hide__menu");
   isMenuHidden = true;
-//   slides.style.transform = `translateY(-${100}vh)`;
-//   activeSlideIndex = 1;
-//   scrollUp.classList.remove("hidden");
-//   scrollDown.classList.remove("hidden");
-//   navLinksSmall.classList.add("hide__menu");
-//   isMenuHidden = true;
- });
+});
 
- projectsLinkSmall.addEventListener("click", () => {
+projectsLinkSmall.addEventListener("click", () => {
   navLinksSmall.classList.remove("show__menu");
   navLinksSmall.classList.add("hide__menu");
   isMenuHidden = true;
-//   slides.style.transform = `translateY(-${200}vh)`;
-//   activeSlideIndex = 2;
-//   scrollUp.classList.remove("hidden");
-//   scrollDown.classList.remove("hidden");
-//   navLinksSmall.classList.add("hide__menu");
-//   isMenuHidden = true;
- });
+});
 
- skillsLinkSmall.addEventListener("click", () => {
+skillsLinkSmall.addEventListener("click", () => {
   navLinksSmall.classList.remove("show__menu");
   navLinksSmall.classList.add("hide__menu");
   isMenuHidden = true;
-//   slides.style.transform = `translateY(-${300}vh)`;
-//   activeSlideIndex = 3;
-//   scrollUp.classList.remove("hidden");
-//   scrollDown.classList.remove("hidden");
-//   navLinksSmall.classList.add("hide__menu");
-//   isMenuHidden = true;
- });
+});
 
- contactLinkSmall.addEventListener("click", () => {
+contactLinkSmall.addEventListener("click", () => {
   navLinksSmall.classList.remove("show__menu");
   navLinksSmall.classList.add("hide__menu");
   isMenuHidden = true;
-//   slides.style.transform = `translateY(-${400}vh)`;
-//   activeSlideIndex = 4;
-//   scrollUp.classList.remove("hidden");
-//   scrollDown.classList.add("hidden");
-//   navLinksSmall.classList.add("hide__menu");
-//   isMenuHidden = true;
- });
+});
 
-// const changeSlide = (direction) => {
-//   if (direction === "down") {
-//     activeSlideIndex++;
-//     if (activeSlideIndex > 0) {
-//       scrollUp.classList.remove("hidden")
-//        navBar.classList.remove("hidden")
-//        navBar.classList.add("show__nav__down")
-//        navBar.classList.remove("show__nav__up")
-
-//     }
-//     if (activeSlideIndex === 4) scrollDown.classList.add("hidden");
-//     if (activeSlideIndex > slidesLength - 1)
-//       activeSlideIndex = slidesLength - 1;
-//   } else if (direction === "up") {
-//     activeSlideIndex--;
-//     if (activeSlideIndex === 0) {
-//       scrollUp.classList.add("hidden");
-//        navBar.classList.remove("show__nav__down")
-//        navBar.classList.add("show__nav__up")
-//        navBar.classList.add("hidden")
-//     }
-//     if (activeSlideIndex < 4) scrollDown.classList.remove("hidden");
-//     if (activeSlideIndex < 0) activeSlideIndex = 0;
-//   }
-//   slides.style.transform = `translateY(-${activeSlideIndex * 100}vh)`;
-// };
-
-const aboutObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("about__show");
-    } else {
-      entry.target.classList.remove("about__show");
-    }
+  const aboutObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("about__show");
+      } else {
+        entry.target.classList.remove("about__show");
+      }
+    });
   });
-});
 
-const aboutElement = document.querySelectorAll(".about");
-aboutElement.forEach((el) => aboutObserver.observe(el));
+  const aboutElement = document.querySelectorAll(".about");
+  aboutElement.forEach((el) => aboutObserver.observe(el));
 
-const skillObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("skill__show");
-    } else {
-      entry.target.classList.remove("skill__show");
-    }
+  const skillObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("skill__show");
+      } else {
+        entry.target.classList.remove("skill__show");
+      }
+    });
   });
-});
 
-const skillElement = document.querySelectorAll(".skill__container");
-skillElement.forEach((el) => skillObserver.observe(el));
+  const skillElement = document.querySelectorAll(".skill__container");
+  skillElement.forEach((el) => skillObserver.observe(el));
 
-const contactObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("contact__show");
-    } else {
-      entry.target.classList.remove("contact__show");
-    }
+  const contactObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("contact__show");
+      } else {
+        entry.target.classList.remove("contact__show");
+      }
+    });
   });
-});
 
-const contactElement = document.querySelectorAll(".contact__info");
-contactElement.forEach((el) => contactObserver.observe(el));
+  const contactElement = document.querySelectorAll(".contact__info");
+  contactElement.forEach((el) => contactObserver.observe(el));
 
-const projectsSwiper = new Swiper(".projects__swiper", {
-  // Optional parameters
-  effect: "cards",
-  centeredSlides: true,
-  slidesPreView: "auto",
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+  const projectsSwiper = new Swiper(".projects__swiper", {
+    // Optional parameters
+    effect: "cards",
+    centeredSlides: true,
+    slidesPreView: "auto",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
-const SkillsSwiper = new Swiper(".skills__swiper", {
-  // Optional parameters
-  effect: "coverflow",
-  centeredSlides: true,
-  slidesPreView: "auto",
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+  const SkillsSwiper = new Swiper(".skills__swiper", {
+    // Optional parameters
+    effect: "coverflow",
+    centeredSlides: true,
+    slidesPreView: "auto",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
